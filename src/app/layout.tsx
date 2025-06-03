612e3cb8d5e8f636eea3fa0ddc81741c737efea2
Mayank Singhal <68636590+MynkSinghal@users.noreply.github.com>
2025-06-04 01:54:35 +0530

    added GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,7 +47,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable} ${libreCaslonText.variable}`}>
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PV683G9X');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body suppressHydrationWarning className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PV683G9X"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <ClientBody>{children}</ClientBody>
         <Analytics />
       </body>
